02d0aaf11bc48fbca6dbad21982248ff57af3fa2
unknowncoderboy <47433622+unknowncoderboy@users.noreply.github.com>
2023-04-05 06:30:46 +0100

    Delete pythonProject directory

--- pythonProject/list tutorial.py
+++
@@ -1,6 +0,0 @@
-cot = 0
-heights = [189, 180, 200, 187, 156, 165, 176, 123, 164, 189, 189, 172, 170, 153, 180]
-for height in heights:
-    if height > 188:
-        cot+=1
-        print(cot)
--- pythonProject/practice.py
+++
@@ -1,5 +0,0 @@
-character_name= "okiki "
-character_age= "5"
-print("there was a boy named " + character_name +", ")
-print("he was " + character_age+", year old ")
-print("he has a room but he likes our room better")
